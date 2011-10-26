6156ffe89dc69688c6e0e660ca2706f8b053a6a3
John Goodsen <jgoodsen@radsoft.com>
2011-10-25 19:53:37 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoodsenOrg::Application.config.secret_token = '0020b413a59f37064d3c6ddc0ce860506171c9c5e20a548fca6eb00d8f4d56829f22ff228814353b7884c4e803eb747251498a411e7635d198cb1e694058d707'
